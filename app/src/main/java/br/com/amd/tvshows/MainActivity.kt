cf9a596cc1c11d2934081941feb9808bd016c099
aldomddev <aldomddev@gmail.com>
2022-10-09 09:55:36 -0300

    Initial Material 3 theming and colors

--- app/src/main/java/br/com/amd/tvshows/MainActivity.kt
+++ app/src/main/java/br/com/amd/tvshows/MainActivity.kt
@@ -1,9 +1,12 @@
 package br.com.amd.tvshows
 
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,11 +24,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        setNavController()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+//        val navView: BottomNavigationView = binding.navView
+//
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_shows,
+//                R.id.navigation_search,
+//                R.id.navigation_favorites
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+//        actionBar?.setHomeButtonEnabled(true)
+    }
+
+    private fun setNavController() {
+        val navHost = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_shows,
@@ -33,13 +52,24 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_favorites
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        actionBar?.setHomeButtonEnabled(true)
+
+        binding.toolbar.setupWithNavController(navHost.navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navHost.navController)
+        navHost.navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.isVisible =
+                appBarConfiguration.topLevelDestinations.contains(destination.id)
+        }
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navHost.navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_options_menu, menu)
+        return true
     }
 }
